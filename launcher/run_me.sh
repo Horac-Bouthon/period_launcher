66fc1672636eb2c9fd6ac30412fcb23babb4a948
tbw <tbrown.wolf@ubk.cz>
2019-12-12 11:18:25 +0100

    relative in run_me

--- launcher/run_me.sh
+++ launcher/run_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-source /home/tbw/workspace/python/period_launcher/.env/bin/activate && /home/tbw/workspace/python/period_launcher/.env/bin/python3 main.py
+source ../.env/bin/activate && ../.env/bin/python3 main.py
